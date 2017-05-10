be8796e0347fe6725e614c439e34ca5f96d67605
Muhammad <Muhammad@MUHAMMAD>
2017-05-10 14:43:37 +0930

    Add project files.

---
+++ OnlineJobs/Controllers/JobsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineJobs;
+
+namespace OnlineJobs.Controllers
+{
+    public class JobsController : Controller
+    {
+        private JobDBEntities2 db = new JobDBEntities2();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            var jobs = db.Jobs.Include(j => j.AspNetUser);
+            return View(jobs.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_ID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Job_Title,Description,Company,Start_Level,Salary,Post_Date,Last_Date,User_ID")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_ID = new SelectList(db.AspNetUsers, "Id", "Email", job.User_ID);
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_ID = new SelectList(db.AspNetUsers, "Id", "Email", job.User_ID);
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Job_Title,Description,Company,Start_Level,Salary,Post_Date,Last_Date,User_ID")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_ID = new SelectList(db.AspNetUsers, "Id", "Email", job.User_ID);
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = db.Jobs.Find(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Job job = db.Jobs.Find(id);
+            db.Jobs.Remove(job);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
